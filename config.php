731e7638b7a4919a7f69dbd66aad910b064e368c
Konrad <konradgaik@icloud.com>
2021-04-09 08:54:22 +0200

    step5/6

--- config.php
+++ config.php
@@ -9,4 +9,6 @@ $conf->server_url = 'http://'.$conf->server_name;
 $conf->app_root = '/GAIK_KALKULATOR_PAW1';
 $conf->app_url = $conf->server_url.$conf->app_root;
 
+$conf->action_root = $conf->app_root.'/app/ctrl.php?action=';
+$conf->action_url = $conf->server_url.$conf->action_root;
 ?>
